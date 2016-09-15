db1fa97359a3d906e93da97f9b1554b50f864971
jimmylin192 <linxin@gmail.com>
2016-09-15 03:41:33 -0500

    more on inliers

--- p1_feamat/matchComparison.m
+++ p1_feamat/matchComparison.m
@@ -12,8 +12,12 @@ close all;
 % THRESHOLD
 TNN_THRESHOLD = 0.8;
 TRT_THRESHOLD = 0.6;
+INL_ITERATIONS = 150;
+INL_THRESHOLD = 3;
 fprintf('Threshold for Thresholed nearest neigbhour: %f.\n ', TNN_THRESHOLD)
 fprintf('Threshold for Thresholed ratio test: %f.\n ', TRT_THRESHOLD)
+fprintf('Number of Iterations for INLIERS CHECK: %d.\n ', INL_ITERATIONS)
+fprintf('Threshold for INLIERS CHECK: %f.\n ', INL_THRESHOLD)
 
 % Some flags
 DISPLAY_PATCHES = 1;
@@ -87,11 +91,11 @@ for scenenum = 1:length(scenenames)
         aSingleSIFTDescriptor = d1(:, im1_des_index);
         dists = dist2(double(aSingleSIFTDescriptor)', double(d2)');
         [sortedDists, sortedIndices] = sort(dists, 'ascend');
-        im1_des_info = [im1_des_index; sortedIndices(1); sortedDists(1)];
-        allMatchMatrix = [allMatchMatrix im1_des_info];
+        match_info = [im1_des_index; sortedIndices(1); sortedDists(1)];
+        allMatchMatrix = [allMatchMatrix match_info];
         % ratio tests
         if (sortedDists(1) <= TRT_THRESHOLD * sortedDists(2))
-            TRTmatchMatrix = [TRTmatchMatrix im1_des_info];
+            TRTmatchMatrix = [TRTmatchMatrix match_info];
         end
     end
     mean_dist = mean(allMatchMatrix(3, :));
@@ -101,7 +105,45 @@ for scenenum = 1:length(scenenames)
             TNNmatchMatrix = [TNNmatchMatrix allMatchMatrix(:,match_index)];
         end
     end
-    
+
+    INLmatchMatrix = [];
+    max_inliers = 0;
+    for INL_ITER = 1:INL_ITERATIONS
+        randomIndices = randperm(n1);
+        randomIndices_d2 = randperm(n2);
+        threeSIFTDescriptors = d1(:, randomIndices(1:3));
+        %trans_template_frame_pos = [];
+        %for index = 1 : 3
+        %    dists = dist2(double(threeSIFTDescriptors(:,index))', double(d2)');
+        %    [sortedDists, sortedIndices] = sort(dists, 'ascend');
+        %    trans_template_frame_pos = [trans_template_frame_pos f2(1:2, sortedIndices(1))];
+        %end
+        scene_frame_pos = f1(1:2, randomIndices(1:3));
+        trans_template_frame_pos = f2(1:2, randomIndices_d2(1:3));
+        tform = fitgeotrans(trans_template_frame_pos', scene_frame_pos', 'affine');
+        trans_f1 = transformPointsForward(tform, double(f1(1:2, :))');
+        inliers = 0;
+        for index = 1 : size(trans_f1, 1)
+            dists = dist2(double(trans_f1(index, :)), double(f2(1:2, :))');
+            min_dist = min(dists);
+            if (min_dist <= INL_THRESHOLD)
+                inliers = inliers + 1;
+            end
+        end
+        if (inliers > max_inliers)
+            max_inliers = inliers;
+            best_transform = tform;
+        end
+    end
+    trans_f1 = transformPointsForward(best_transform, f1(1:2, :)');
+    for index = 1 : size(trans_f1, 1)
+        dists = dist2(double(trans_f1(index, :)), double(f2(1:2, :))');
+        [sortedDists, sortedIndices] = sort(dists, 'ascend');
+        if (sortedDists(1) <= INL_THRESHOLD)
+            match_info = [index; sortedIndices(1); sortedDists(1)];
+            INLmatchMatrix = [INLmatchMatrix match_info];
+        end
+    end
     % We have just one match here, but to use the display functions below, you
     % can simply expand this matrix to include one column for each match.
     numMatches = size(TNNmatchMatrix,2);
@@ -109,28 +151,36 @@ for scenenum = 1:length(scenenames)
     % Display the matched patch
     clf;
     showMatchingPatches(TNNmatchMatrix, d1, d2, f1, f2, im1, im2, SHOW_ALL_MATCHES_AT_ONCE);
-    fprintf('Showing an example of %s:TNN patch match. \n', scenenames(scenenum));
+    fprintf('Showing an example of %s,TNN patch match. \n', scenenames{scenenum});
     keyboard;
     
     % An alternate display - show lines connecting the matches (no patches)
-    % Allows you to visualize the smoothness of the connections without
-    % clutter of the patches.
     clf;
     showLinesBetweenMatches(im1, im2, f1, f2, TNNmatchMatrix);
-    fprintf('Showing the %s:TNN matches with lines connecting.\n', scenenames(scenenum));
+    fprintf('Showing the %s,TNN matches with lines connecting.\n', scenenames{scenenum});
     keyboard;
 
     % Display the matched patch
     clf;
     showMatchingPatches(TRTmatchMatrix, d1, d2, f1, f2, im1, im2, SHOW_ALL_MATCHES_AT_ONCE);
-    fprintf('Showing an example of %s:TRT patch match. \n', scenenames(scenenum));
+    fprintf('Showing an example of %s:TRT patch match. \n', scenenames{scenenum});
     keyboard;
     
     % An alternate display - show lines connecting the matches (no patches)
-    % Allows you to visualize the smoothness of the connections without
-    % clutter of the patches.
     clf;
     showLinesBetweenMatches(im1, im2, f1, f2, TRTmatchMatrix);
-    fprintf('Showing the %s:TRT matches with lines connecting.\n', scenenames(scenenum));
+    fprintf('Showing the %s:TRT matches with lines connecting.\n', scenenames{scenenum});
+    keyboard;
+
+    % Display the matched patch
+    clf;
+    showMatchingPatches(INLmatchMatrix, d1, d2, f1, f2, im1, im2, SHOW_ALL_MATCHES_AT_ONCE);
+    fprintf('Showing an example of %s:INL patch match. \n', scenenames{scenenum});
+    keyboard;
+    
+    % An alternate display - show lines connecting the matches (no patches)
+    clf;
+    showLinesBetweenMatches(im1, im2, f1, f2, INLmatchMatrix);
+    fprintf('Showing the %s:INL matches with lines connecting.\n', scenenames{scenenum});
     keyboard;
 end
